f07e31bbcaae9936496656e24443121f19d1f8a0
wataru1999 <pikapii25@gmail.com>
2022-02-21 23:58:52 +0800

    Delete Modal Done

--- resources/views/employees.blade.php
+++ resources/views/employees.blade.php
@@ -184,9 +184,53 @@
 				  <button class="bg-yellow-400 px-3 py-2 text-sm shadow-sm font-medium tracking-wider border text-black rounded-full hover:shadow-lg hover:bg-yellow-500" @click="showModal4 = true">
                     Edit
                   </button>
-                  <button class="bg-red-400 px-3 py-2 text-sm shadow-sm font-medium tracking-wider border text-black rounded-full hover:shadow-lg hover:bg-red-500" @click="showModal4 = true">
+                  <button class="bg-red-400 px-3 py-2 text-sm shadow-sm font-medium tracking-wider border text-black rounded-full hover:shadow-lg hover:bg-red-500" onclick="toggleModal('modal-id')">
                     Delete
                   </button>
+				  
+				  <div class="hidden overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none justify-center items-center" id="modal-id">
+					<div class="relative w-auto my-6 mx-auto max-w-lg">
+					  <!--content-->
+					  <div class="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
+						<!--header-->
+						<div class="flex items-start justify-between pl-5 pr-10 pb-3 pt-3 border-b border-solid border-blueGray-200 rounded-t">
+						  <h3 class="text-3xl font-semibold">
+							Delete this entry?
+						  </h3>
+						  <button class="p-1 ml-auto bg-transparent border-0 text-black opacity-5 float-right text-3xl leading-none font-semibold outline-none focus:outline-none" onclick="toggleModal('modal-id')">
+							<span class="bg-transparent text-black opacity-5 h-6 w-6 text-2xl block outline-none focus:outline-none">
+							  ×
+							</span>
+						  </button>
+						</div>
+						<!--body-->
+						<div class="relative p-3 flex-auto">
+						  <p class="my-4 text-blueGray-500 text-lg leading-relaxed">
+							Are you sure to delete this entry?
+						  </p>
+						</div>
+						<!--footer-->
+						<div class="flex items-center justify-end p-6 border-t border-solid border-blueGray-200 rounded-b">
+						  <button class="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150" type="button" onclick="toggleModal('modal-id')">
+							Cancel
+						  </button>
+						  <button class="bg-red-500 text-white active:bg-emerald-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150" type="button" onclick="toggleModal('modal-id')">
+							Delete
+						  </button>
+						</div>
+					  </div>
+					</div>
+				  </div>
+				  <div class="hidden opacity-25 fixed inset-0 z-40 bg-black" id="modal-id-backdrop"></div>
+				  <script type="text/javascript">
+					function toggleModal(modalID){
+					  document.getElementById(modalID).classList.toggle("hidden");
+					  document.getElementById(modalID + "-backdrop").classList.toggle("hidden");
+					  document.getElementById(modalID).classList.toggle("flex");
+					  document.getElementById(modalID + "-backdrop").classList.toggle("flex");
+					}
+				  </script>
+
 								</td>
 						@endforeach
 					</table>
