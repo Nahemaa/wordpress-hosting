50a13e7d88edeea9992b493e4ab0258979c9ad70
Dave_Carlos <dave.carlos.atgs@gmail.com>
2022-03-18 23:48:10 +0800

    80%?

--- app/Http/Controllers/employeesController.php
+++ app/Http/Controllers/employeesController.php
@@ -213,8 +213,16 @@ return view('employees', ['employees'=>$employees]);
 
     public function EditUser($id) {
 
+        
         $departments = DB::table("departments")->pluck("department_name", "id");
-        $employees = User::find($id);
+        $employees = User::find($id)
+                            ->join('departments', 'departments.id', '=', 'users.department')
+                            ->join('job_level', 'job_level.id', '=', 'users.job_level')
+                            ->join('job_position', 'job_position.id', '=', 'users.job_position')
+                            ->get(['users.id', 'users.employee_id', 'users.last_name', 'users.first_name', 'users.email', 'departments.department_name', 'job_level.level_name', 'job_position.position_name', 'users.created_at']);
+
+        
+        ;
         return view('users.edituser', compact('employees', 'departments'));
 
     }
--- resources/views/users/edituser.blade.php
+++ resources/views/users/edituser.blade.php
@@ -147,16 +147,14 @@
                   <div class="relative inline-flex">
                     <svg class="w-2 h-2 absolute top-0 right-1 m-3 pointer-events-none" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 412 232"><path d="M206 171.144L42.678 7.822c-9.763-9.763-25.592-9.763-35.355 0-9.763 9.764-9.763 25.592 0 35.355l181 181c4.88 4.882 11.279 7.323 17.677 7.323s12.796-2.441 17.678-7.322l181-181c9.763-9.764 9.763-25.592 0-35.355-9.763-9.763-25.592-9.763-35.355 0L206 171.144z" fill="#648299" fill-rule="nonzero"/></svg>
                     <select name="department" id="department" class="border border-black-300 rounded-full text-black-600 h-10 pl-5 pr-10 bg-white hover:border-black-400 focus:outline-none appearance-none">
-                      <option value="" selected disabled>Select Department</option>
-                      @foreach ($departments->department as $department)
+                      <option value="" selected disabled>{{$employees->department}}</option>
 
-                            <option value="{{ $department->department}}" selected>
-                                {{$department->department}}
+
+                      @foreach ($departments as $key => $department)
+                            <option value="{{ $key }}">
+                                {{$department}}
                             </option>
-
-
                        @endforeach
-
                     </select>
                   </div>                
                 </div>
@@ -171,7 +169,7 @@
                   <div class="relative inline-flex">
                     <svg class="w-2 h-2 absolute top-0 right-1 m-3 pointer-events-none" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 412 232"><path d="M206 171.144L42.678 7.822c-9.763-9.763-25.592-9.763-35.355 0-9.763 9.764-9.763 25.592 0 35.355l181 181c4.88 4.882 11.279 7.323 17.677 7.323s12.796-2.441 17.678-7.322l181-181c9.763-9.764 9.763-25.592 0-35.355-9.763-9.763-25.592-9.763-35.355 0L206 171.144z" fill="#648299" fill-rule="nonzero"/></svg>
                     <select name="job_level" id="job_level" class="border border-black-300 rounded-full text-black-600 h-10 pl-5 pr-10 bg-white hover:border-black-400 focus:outline-none appearance-none">
-                      <option> Select Job Level </option>
+                      <option> {{$employees->job_level}} </option>
                     </select>
                   </div>                
                 </div>
@@ -186,7 +184,7 @@
                   <div class="relative inline-flex">
                     <svg class="w-2 h-2 absolute top-0 right-1 m-3 pointer-events-none" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 412 232"><path d="M206 171.144L42.678 7.822c-9.763-9.763-25.592-9.763-35.355 0-9.763 9.764-9.763 25.592 0 35.355l181 181c4.88 4.882 11.279 7.323 17.677 7.323s12.796-2.441 17.678-7.322l181-181c9.763-9.764 9.763-25.592 0-35.355-9.763-9.763-25.592-9.763-35.355 0L206 171.144z" fill="#648299" fill-rule="nonzero"/></svg>
                     <select name="job_position" id="job_position" class="border border-black-300 rounded-full text-black-600 h-10 pl-5 pr-10 bg-white hover:border-black-400 focus:outline-none appearance-none">
-                      <option>Select Job Position</option>
+                      <option>{{$employees->job_position}}</option>
                    
                     </select>
                   </div>                
